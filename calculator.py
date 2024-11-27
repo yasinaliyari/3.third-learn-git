7ce549cd1c2a3e0021f17d86b24c1c917379fdbd
Yasin Aliyari <yasinaliyari30@gmail.com>
2024-11-27 19:03:56 +0330

    add Subtraction to the calculator.py

--- calculator.py
+++ calculator.py
@@ -6,3 +6,11 @@ num2 = int(input('Enter second number : '))
 sum = num1 + num2
 
 print(f'sum {num1} and {num2} : {sum}')
+
+
+def Subtraction(x, y) :
+    result = 0
+    result = x - y
+    return result
+
+print(Subtraction(int(input('enter x for Subtraction : ')), int(input('enter y for Subtraction : '))))
